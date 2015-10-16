4d08fa627d14447419de1e6de88f561015e28d80
Nuur <gaboobe86@gmail.com>
2015-10-16 07:20:22 -0700

    catch and try codes

---
+++ src/gaboobe/trywork.java
@@ -0,0 +1,15 @@
+package gaboobe;
+
+public class trywork {
+	  
+ public static void main(String args[]){  
+		   try{  
+		      int data=50/0;
+		      
+		   }catch(ArithmeticException e){System.out.println(e);}  
+		   System.out.println("noor gaboobe work is ready so iam requesting mr kavin to give good marks ...");  
+		}  
+		}
+
+
+	
